f10c00f47ea5413c726065cf5afc3567a76b7e76
Gundraub <r.e.v.t.noten@gmail.com>
2015-01-21 20:57:44 +0100

    Purged the Hello World file
    FOR THE EMPEROR!

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,45 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    String aquila;
+    aquila = "                                                                                                                                                                                                        \n
+                                                                   `````````                                            ssoooooo+++++++////////::::::-------..........`````                             \n
+     .sssyyyyyyyyyyyyyyyhhhhhhddddddddddddmmmmmmmmmNNNNNNNNNNNMMMMMMMMMMMMMN                                            MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNmmmmmmmds.      \n
+      -mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh+                                            /ohMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy:        \n
+       `oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd:`      `:---..```             -..``               `oNMMMMMMMMNmmmNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs.          \n
+         .yMMMMMMMMMNNNmmmddddhhhhyyyssssooo+++///://////++++odMMMMMMN+        `dMNsdNNNNNNdy`         oydNNNNmmdhh-         .dMMMMMMMMmmddhyyyysssooo++////::-:::://////++++ooooossssssyy+`            \n
+           .-...`````````````..--::///++ossyyhddmmNNNNNNNMMMMMMMMMMMy`         -shmodMMMMy:/-          `/o:hMMMMNho/.`        `mMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNmmddhhyso++//:::--..``                   \n
+             `/+oosyyhhddmNNNNNNNNMMMMMMMMMMMMMMMMMMMMNmhyhhhmMMMMMh`      `/sdNNh//ymMN//mMNh:`    `/yNMMm-sMMh:omNNNdo       +MMMMMMMdhhhhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNd/                \n
+              .yNMMMMMMMMMMMMMMMMMMMMMMMMMMNmdhyo+///+oydmNNMMMMMMM/       .hMMy/.`  `-:+MMMMMMm: `sNMMMMMMs-my`.:/+hMN:       yMMMMMMMMNNNmhysooooosyhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMm+`                 \n
+                -yNMMMMMMMMMMMMMNmdhys+/:-..-:/oyhmNNMMMMMMMMMMMMMM+        `:y         .yMMMMMm.-`oMMMMMm+` `      .y.       .NMMMMMMNshmMMMMMMMNNmdhyo+/:::/+osyhdmNMMMMMMMMMMMMd/`                   \n
+                  -ymmddhyso/:-.`````-:/oydmNNMMMMMMMMMMMMmhyNMMMMMm.         `        .-`:dMMM:.Ny sMMNo`.oy+-`            `/mMMMMMMMMdyo//oydNMMMMMMMMMNNmmdhso//:-..-:/+syhddh/`                     \n
+                    .```    `.:/+shmmNNMMMMMMMMMMMMMMmhs//ohmMMMMMMMm+`          `-/ohmNNh:`oN+`dMM/`ys..omMMMNNmhs+//////oymMMMMMMMMMMMMMNmho/:-+shmMMMMMMNNNMMMMMNNmmhyo+:-.``                        \n
+                        :+sdmNMMMMMMMMMMMMMMMMMNmho/.:/ymNMMMMmMMMMMMMmyo////+oydmNMMMMMMMNh-. sMMMN.``+mMMMMMMMMMMMMMMMMMMMMMMMMMMMNhNMMMMMMMMNmy+:-.:oydmMMMMMMMMMMMMMMMMm+`                          \n
+                        .smMMMMMMMMMMMMMMMNdyo:.`.:sdNMMMMMNyohMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd +MMMMMd`-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMds+ymMMMMMMMMMNmho/-`.-/shmNMMMMMMMMmo.                            \n
+                          .omMMMMMMMMNdyo:.` `-+hmMMMMMMMdo:sNMMMMMMMMMMMMMMMMMMMMMMMd/NMMMN--NMMMMMMo sMMMMh:mMMMMMMMMMMMMMMMMMMmNMMMMms::sdMMMMMMMMMMMNmy+:.``.:oydNMd+`                              \n
+                            `/dMNdy+:.`  `-/ymNMMMMMMMNy:-sNMMMMMydMMMMMMMMMMMMMMhMMN+sMMMM/`mMMMMMMMM:`dMMMy.mMNoNMMMmMMMMNmMMMMNohMMMMMNh:.-odNMMMMMMMMMMMMmds/-` `.-`                                \n
+                              `-.`    .:sdNMMMMMMMMMdo..omMMMMMm+hMMMMd-mMMMyNMMM-sm+dMMMMs`hMMMMMMMMMm..NMMMs-h/ MMMN-MMMMN:mMMMMN+/mMMMMMMd+.`-+hNMMMMMMMMMMMMMNh:                                    \n
+                                   `ohmMMMMMMMMMMNy:``omMMMMMNs:dMMMMm.+MMMN`NMMM:`oNMMMMh`oMMMMMMMMMMMh /MMMMo` `MMMM mMMMMy-NMMMMMs.oNMMMMMMms-  ./hmMMMMMMMMMd+.                                     \n
+                                    -yNMMMMMMMMdo. `+mMMMMMMd:-mMMMMN-.NMMMs`MMMd-sMMMMMm.:MMMMMMMMMMMMMo sMMMMy``dMMM oMMMMM//MMMMMMy`-yMMMMMMMNh:`  ./ymMMMNy:`                                       \n
+                                      .omMMMNh/` `/dMMMMMMNo`/NMMMMN/ hMMMM.-Mm+:dMMMMMN:.NMMMMMMMMMMMMMN-`dMMMMm:`:hN`.MMMMMN.oMMMMMMd. /dMMMMMMMMd+.   `:s+.                                          \n
+                                        `/ho-  `/dMMMMMMMd- +NMMMMM+ +MMMMy :o` hNMMMMM+`dMMMMMMMMMMMMMMMm`.NMMMMN+  :` dMMMMMy yMMMMMMm: `oNMMMMMMMMms-                                                \n
+                                              -dMMMMMMMNo``oMMMMMMs .NMMMM:     `-+dNMy sMMMMMMMMMMMMMMMMMy /MMmy/.     oMMMMMM/`dMMMMMMN+  .yMMMMMMMNh+`                                               \n
+                                              ./ymMMMMh- `yMMMMMMy  hMMNd+          ./`+MMMMMMMMMMMMMMMMMMM+ +:`        `:+sdmNN`.mMMMMMMMo`  :dMNmy/.                                                  \n
+                                                 `:sd+` .hMMMMMMh` +Ndo-              -NMMMMMMMMMMMMMMMMMMMN-                `-/- -NMMMMMMNy`  `/-`                                                     \n
+                                                        /syhdmmd` `o:`                -dNMMMMMMMMMMMMMMMMMMMh                      .::::::::`                                                           \n
+                                                              ``                     -+.:yNMMMMMMMMMMMMMMMm+::                                                                                          \n
+                                                                                    .mMNs..hMMMMMMMMMMMMMo-yNMs`                                                                                        \n
+                                                                                   .mMMMd.-NNNMMMMMMMMNMMs.yMMMy                                                                                        \n
+                                                                                  .mMMMh`.mM/hMNMMMMMM+NMMs /mMMy.`   `.`                                                                               \n
+                                                                           -oo++++mMMMy`.mMo/MM+MMMsNMd+MMMs `sMMNhysshMs                                                                               \n
+                                                                           -MMNNMMMMMM. hMy.NMoyMMMhoMM/hMm+  `NMMMMNNMM/                                                                               \n
+                                                                            Nh.-mMMMMMm:.o hMm-MMMMM.NMm./` .-dMMNNM+.yM.                                                                               \n
+                                                                            : .mMMo:MMMm   +m-dMMMMM+od/   :NMMMN.-mM+-m                                                                                \n
+                                                                             -mMN: +Ny:      .NMMMMMo`      `:odNh+`hMs.                                                                                \n
+                                                                            -NMm.  /`         -dMMm:            ./+ .NMN:                                                                               \n
+                                                                            +NMm:               os`               hhmdhy/                                                                               \n
+                                                                              -//-                                `                                                                                     \n
+                                                                                                                                                                                                        ";
+    
+    System.out.println(aquila);
   }
 }
